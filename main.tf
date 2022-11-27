b7d60059966454a058956a729c7d7ddaefb89502
Dominic Olajire <40606972+olajiredominic@users.noreply.github.com>
2022-11-28 00:06:51 +0100

    Point backend to correct TFC org and workspace

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  cloud {
+    organization = "REPLACE_ME"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
