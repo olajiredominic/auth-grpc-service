cf11b8b06847c0b0c2ef6815a36901f2f89f5ce5
Dominic Olajire <40606972+olajiredominic@users.noreply.github.com>
2022-11-28 07:25:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.26.0"
     }
     random = {
       source = "hashicorp/random"
-      version = "3.0.1"
     }
   }
   required_version = ">= 1.1.0"
