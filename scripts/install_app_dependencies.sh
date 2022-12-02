f29b0af6c574cd1a322750c9cdd8ecbae49b4309
Dominic Olajire <40606972+olajiredominic@users.noreply.github.com>
2022-12-02 06:14:55 +0100

    Update install_app_dependencies.sh

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -3,6 +3,6 @@ echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
 source ~/.profile
 
 cd /home/ubuntu
-chmod +w ./auth_app
+chmod 777 -R ./auth_app
 cd ./auth_app
 go build -o auth_app ./cmd
