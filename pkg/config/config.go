235dda33c143678732e043d843bbf75f8ffc4b1d
Olajire Dominic <lerryjay45@gmail.com>
2022-12-03 17:32:13 +0100

    changed env keys

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,13 +5,13 @@ import (
 )
 
 type Config struct {
-	Port       string `mapstructure:"PORT"`
-	DBURL      string `mapstructure:"DB_URL"`
-	DBUSER     string `mapstructure:"DB_USER"`
-	DBPWD      string `mapstructure:"DB_PWD"`
+	Port       string `mapstructure:"AUTH_SVC_PORT"`
+	DBURL      string `mapstructure:"AUTH_DB_URL"`
+	DBUSER     string `mapstructure:"AUTH_DB_USER"`
+	DBPWD      string `mapstructure:"AUTH_DB_PWD"`
 	JWT_SECRET string `mapstructure:"JWT_SECRET"`
-	APP_NAME   string `mapstructure:"JWT_SECRET"`
-	APP_URL    string `mapstructure:"JWT_SECRET"`
+	APP_NAME   string `mapstructure:"APP_NAME"`
+	APP_URL    string `mapstructure:"APP_URL"`
 }
 
 func LoadConfig() (config Config, err error) {
@@ -31,11 +31,12 @@ func LoadConfig() (config Config, err error) {
 
 	// TODO: Set config for permission type possibly in env
 	config = Config{
-		Port:    os.Getenv("Port"),
-		DBURL:   os.Getenv("DB_URL"),
-		DBUSER:  os.Getenv("DB_USER"),
-		DBPWD:   os.Getenv("DB_PWD"),
-		APP_URL: os.Getenv("APP_URL"),
+		Port:       os.Getenv("AUTH_SVC_PORT"),
+		DBURL:      os.Getenv("AUTH_DB_URL"),
+		DBUSER:     os.Getenv("AUTH_DB_USER"),
+		DBPWD:      os.Getenv("AUTH_DB_PWD"),
+		APP_URL:    os.Getenv("APP_URL"),
+		JWT_SECRET: os.Getenv("JWT_SECRET"),
 	}
 
 	return config, err
