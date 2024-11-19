6a79fce1721444931a284c362fbcac0db03d003f
Olajire Dominic <lerryjay45@gmail.com>
2024-11-19 11:44:21 +0100

    fix: query error for user permission update

--- pkg/routes/auth.go
+++ pkg/routes/auth.go
@@ -331,7 +331,7 @@ func (h *Handler) UpdateUserPermissions(ctx context.Context, req *pb.UpdateUserP
 
 	for _, v := range removed {
 		var permission models.UserPermissionORM
-		h.DB.First(&permission, "user_id = ? OR permission = ? OR status = ?", req.UserId, v, int32(models.Status_INACTIVE))
+		h.DB.First(&permission, "user_id = ? AND permission = ? AND status = ?", req.UserId, v, int32(models.Status_ACTIVE))
 
 		permission.UserId = &req.UserId
 		permission.Permission = v
